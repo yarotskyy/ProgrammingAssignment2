6a310a007e90c7974d7b365f54180279504455fa
yarotskyy <yarotskyy@yahoo.com>
2017-02-28 22:33:24 -0500

    .R file with functions edited

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates makeCachMatrix object - matrix
 
-## Write a short comment describing this function
+## It also checks whether the inverse matrix is cached or not.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse matrix ###
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m        
+## Return a matrix that is the inverse of 'x'
 }
+
+##########################################
+## How it works
+## Step 1. Create a matrix x1
+##    x1<-matrix(c(0.5,-0.25,-1,0.75),2,2)
+## Step2. call makeCacheMatrix function
+##    m1 <- makeCacheMatrix(x1)
+## Step3. Make an inverse matrix by calling cacheSolve function
+##    n1 <- cacheSolve(m1)
+## Step4. Print the inverse matrix n1
+##    n1
+###########################################
+###########################################
+## Example
+## > x1<-matrix(c(0.5,-0.25,-1,0.75),2,2)
+## > m1 <- makeCacheMatrix(x1)
+##> n1 <- cacheSolve(m1)
+##> n1
+##[,1] [,2]
+##[1,]    6    8
+##[2,]    2    4
